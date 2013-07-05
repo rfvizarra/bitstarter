68b17a935c7895563605a9f82db27d574a4fc723
Rodrigo Fernandez-Vizarra <rfvizarra@gmail.com>
2013-07-05 22:46:29 +0200

    Fixed an undefined variable. Should test before commit

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var content = fs.readFileSync('index.html');
-    response.send(index.toString('utf8'));
+    response.send(content.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
